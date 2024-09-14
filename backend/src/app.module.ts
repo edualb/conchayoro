f5ee34b695a8446795226a2f094a2056fe8bbf60
edualb <eduardoslafiq@gmail.com>
2024-09-14 14:08:02 +0000

    create products logic

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,11 +1,16 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { ProductsModule } from './products/products.module';
+import { SequelizeModule } from '@nestjs/sequelize';
 
 @Module({
-  imports: [ProductsModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    SequelizeModule.forRoot({
+      dialect: 'sqlite',
+      omitNull: true,
+      autoLoadModels: true,
+      synchronize: true
+    }),
+    ProductsModule,
+  ],
 })
 export class AppModule {}
