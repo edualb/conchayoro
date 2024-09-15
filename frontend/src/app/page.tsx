3c94b455e9c4869ce3c3a54d231fa8d9a6a34334
edualb <eduardoslafiq@gmail.com>
2024-09-15 21:09:01 +0000

    add feature-flag

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,9 +1,15 @@
-export default function Home() {
+import { blackFridayFlag } from './flags';
+
+export default async function Page() {
+  const black_friday = await blackFridayFlag();
+  
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <h1> Welcome to the ConchaYOro App - deploy v2 </h1>
+        <h1> Welcome to the ConchaYOro App - deploy v3 </h1>
         <p> By: Eduardo Albuquerque da Silva </p>
+        
+        { black_friday ? <button> Promo </button> : <button> Normal </button> }
       </div>
     </main>
   )
